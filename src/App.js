1bc772721f004483f64d101e6236beabb3f2e20c
adamjunios <adamjunioselvaa@gmail.com>
2022-07-21 16:28:04 +0700

    update style
    add style to the app using sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
 import { fetchWeather } from "./api/fetchWeather";
+import Header from "./components/Header";
+import "./App.scss";
+import iconLoc from "./assets/location.png";
+import iconEnter from "./assets/enter.png";
 
 const App = () => {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState("");
+  const [barIcon, setBarIcon] = useState(iconLoc);
 
-  const search = async (e) => {
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+    setBarIcon(iconEnter);
+  };
+
+  const handleLeave = (e) => {
+    setBarIcon(iconLoc);
+  };
+
+  const handleSearch = async (e) => {
     if (e.key === "Enter") {
       const getData = await fetchWeather(query);
       console.log(getData);
@@ -16,37 +30,56 @@ const App = () => {
 
   return (
     <>
-      <div className="main-container">
-        <input
-          type="text"
-          className="search"
-          placeholder="search..."
-          value={query}
-          onChange={(e) => {
-            setQuery(e.target.value);
-          }}
-          onKeyPress={search}
-        />
-        {weather.main && (
-          <div className="city">
-            <h2 className="city-name">
-              <span>{weather.name}</span>
-              <sup>{weather.sys.country}</sup>
-            </h2>
-            <div className="city-temp">
-              {Math.round(weather.main.temp)}
-              <sup>&deg;C</sup>
-            </div>
-            <div className="info">
-              <img
-                className="city-icon"
-                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                alt={weather.weather[0].description}
+      <div className="container">
+        <Header />
+        <div className="pages">
+          <div className="result row row_center">
+            {weather.main ? (
+              <div className="ui_card">
+                <div className="weather">
+                  <div className="weather_icon">
+                    <img
+                      src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                      alt={weather.weather[0].description}
+                    />
+                  </div>
+                  <div className="weather_city">
+                    <span>{weather.name}</span>
+                    <sup>
+                      <b>{weather.sys.country}</b>
+                    </sup>
+                  </div>
+                  <div className="weather_temp">
+                    {Math.round(weather.main.temp)}
+                    <sup>&deg;</sup>C
+                  </div>
+                  <div className="weather_desc">
+                    <p>{weather.weather[0].description}</p>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div>Please search location first.</div>
+            )}
+          </div>
+          <div className="search">
+            <div className="bar row row_center">
+              <input
+                type="text"
+                placeholder="Search Location..."
+                value={query}
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+                onMouseLeave={handleLeave}
+                onKeyPress={handleSearch}
               />
-              <p>{weather.weather[0].description}</p>
+              <div className="images row row_center">
+                <img src={barIcon}></img>
+              </div>
             </div>
           </div>
-        )}
+        </div>
       </div>
     </>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="nav">
+          <div className="row row_space">
+            <div className="title">Weather App</div>
+            <div className="menu">i</div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
